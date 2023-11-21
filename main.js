425a5728930e844a38402837da01609cd8524102
prototpeqw <146764866+prototpeqw@users.noreply.github.com>
2023-11-21 17:21:26 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+let str = "hello, owu, com, ua";
+console.log(str)
+let n1 = "1, 10, -999, 123, 3.14, 2.7, 16"
+console.log(n1)
+let n2=10
+let n3 =-999
+let n4 = 123
+let n5 = 3.14
+let n6 = 2.7
+let n7 = 16
+console.log(n1,n2,n3,n4,n5,n6,n7)
+let t = true
+let f = false
+console.log(t,f)
+let firstName = 'Anton'
+let middleName = ' Sergiyovich'
+let lastName = ' Kirichenko'
+let person = firstName + lastName + middleName
+console.log(person)
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+
+
+
+let firstName1 = prompt("Введіть  своє і'мя")
+
+
+let lastName2 = prompt("По-Батькові:");
+
+
+let age3 = prompt("Скільки вам років?:");
+
+
+console.log("Ім'я:", firstName1);
+console.log("По-Батькові:", lastName2);
+console.log("Вік:", age3);
+
+
+
+
+
+
